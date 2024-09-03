b40517dc45f1d1a7364c4aaf943bcfe8aa17816a
Rafael Luz <Rafael.Luz@smanuvemlive.onmicrosoft.com>
2024-09-03 14:28:50 -0300

    Create Controller User

---
+++ src/backend/TaskManagement.API/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagement.Communication.Requests;
+using TaskManagement.Communication.Responses;
+
+namespace TaskManagement.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+	[HttpPost]
+	[ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
+	public IActionResult Register(RequestRegisterUserJson register)
+	{
+		return Created();
+	}
+}
---
+++ src/shared/TaskManagement.Communication/Responses/ResponseRegisteredUserJson.cs
@@ -0,0 +1,6 @@
+﻿namespace TaskManagement.Communication.Responses;
+
+public class ResponseRegisteredUserJson
+{
+    public string Name { get; set; }
+}
